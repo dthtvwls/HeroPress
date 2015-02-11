dce509c9aea711b3ed13ec3ffbdeae279736fa16
Joshua Stauter <joshua@joshuastauter.com>
2015-02-11 04:01:36 -0500

    why isn't coverage sending

--- src/Handlebars.php
+++ src/Handlebars.php
@@ -1,7 +1,7 @@
 <?php
 class Handlebars extends Slim\View {
 
-  var $fileext, $partials = [];
+  var $fileext;
 
   function __construct($fileext = 'hbs') {
     parent::__construct();
@@ -17,20 +17,22 @@ class Handlebars extends Slim\View {
      * if template is relative, we can just get it from the partials
      */
     if ($basedir = $this->getTemplatesDirectory()) {
-      $this->partials = array_reduce(scandir($basedir), function ($carry, $item) use ($basedir) {
+      $partials = array_reduce(scandir($basedir), function ($carry, $item) use ($basedir) {
         $pathinfo = pathinfo($item);
         if ($pathinfo['extension'] === $this->fileext) {
           $carry[$pathinfo['filename']] = file_get_contents("$basedir/$item");
         }
         return $carry;
       });
+    } else {
+      $partials = [];
     }
 
     // use template from partials or read from file
     $renderer = LightnCandy::prepare(LightnCandy::compile(
-      array_key_exists($template, $this->partials) ? $this->partials[$template] : file_get_contents($template), [
+      array_key_exists($template, $partials) ? $partials[$template] : file_get_contents($template), [
         'flags'    => LightnCandy::FLAG_HANDLEBARS,
-        'partials' => $this->partials,
+        'partials' => $partials,
         'helpers'  => ['editable' => 'Handlebars::editable']
       ]
     ));
@@ -40,7 +42,7 @@ class Handlebars extends Slim\View {
 
   static function editable($args) {
     $app = HeroPress::getInstance();
-    
+
     if (!isset($args[1])) $args[1] = $app->select($args[0]);
     if (!isset($args[2])) $args[2] = $app->isLoggedIn();
 
